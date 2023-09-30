34c0536b4622d3453055d62b853bae5d745cad94
Михаил Сим <msim@sfedu.ru>
2023-09-30 10:48:01 +0300

    Изменено предназанчение кода

--- hello.pas
+++ hello.pas
@@ -1,1 +1,11 @@
-﻿## Print('Hello, World!');
+﻿begin
+  var time := ReadInteger('Введите текущий час:');
+  
+  case time of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!')
+  else
+    Print('Доброй ночи, мир!');
+  end;
+end.
